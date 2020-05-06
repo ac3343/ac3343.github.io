e0c884d58c4b90f3b2006c860884bab701389666
Andrew Cambridge <56008700+ac3343@users.noreply.github.com>
2020-05-06 15:54:09 -0500

    Created JP Dictionary README

--- WrestlingRPG/Assets/Scripts/AttackList.cs
+++ WrestlingRPG/Assets/Scripts/AttackList.cs
@@ -6,10 +6,15 @@ public class AttackList
 {
     //List of attacks
     static Attack punch = new Attack("Punch", 4, "Strike", 4);
+    static Attack dropkick = new Attack("Dropkick", 7, "Strike", 6);
 
     //Attack Properties
     public static Attack Punch
     {
         get { return punch; }
+    }
+    public static Attack Dropkick
+    {
+        get { return dropkick; }
     }
 }
