00727c843adeca4702b094c8494c35a6684a1b75
Andrew Cambridge <roronoaandrew@gmail.com>
2020-10-09 16:46:57 -0400

    Midterm: Feat
    Basic movement

---
+++ Midterm/Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerPrefab;
+
+    GameObject cameraObject;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Midterm/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float X
+    {
+        get { return transform.position.x; }
+    }
+    public float Y
+    {
+        get { return transform.position.y; }
+    }
+
+    [SerializeField]
+    float maxSpeed = 0.003f;
+    [SerializeField]
+    float acceleration = 0.0001f;
+
+    Vector2 currentPos;
+    Vector2 prevPos;
+    float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = prevPos = transform.position;
+        velocity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (velocity < maxSpeed)
+            {
+                velocity += acceleration;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (velocity > -maxSpeed)
+            {
+                velocity -= acceleration;
+            }
+        }
+        if (!Input.anyKey)
+        {
+            velocity /= 3;
+        }
+
+        currentPos.x += velocity;
+        transform.position = currentPos;
+        prevPos = currentPos;
+    }
+}
