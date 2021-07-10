7bfdea21307c4ba4792489584cdaab740c58895d
Andrew Cambridge <roronoaandrew@gmail.com>
2021-07-10 18:25:58 -0500

    OPRouge: Feat
    Setup UI Notifications for battle

---
+++ OPRouge/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    NotifContainer notifications;
+    // Start is called before the first frame update
+    void Start()
+    {
+        notifications = FindObjectOfType<NotifContainer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            notifications.AddNotif("wutizgoinerr");
+        }
+    }
+}
---
+++ OPRouge/Assets/Scripts/NotifContainer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotifContainer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject notifPrefab;
+    [SerializeField]
+    private uint Capacity;
+
+    private List<GameObject> currentNotifs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentNotifs = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckNotifs();
+    }
+
+    public void AddNotif(string text)
+    {
+        GameObject newNotif = Instantiate(notifPrefab, this.transform);
+
+        newNotif.GetComponentInChildren<Text>().text = text;
+
+        for (int i = currentNotifs.Count - 1; i >= 0; i--)
+        {
+            if (currentNotifs[i] != null)
+            {
+                Vector3 notifPos = currentNotifs[i].transform.position;
+                currentNotifs[i].transform.position = new Vector3(notifPos.x, notifPos.y + 60);
+            }
+
+        }
+
+        currentNotifs.Add(newNotif);
+    }
+
+    void CheckNotifs()
+    {
+        for(int i = currentNotifs.Count - 1; i >= 0; i--)
+        {
+            if(currentNotifs[(int)i] == null)
+            {
+                currentNotifs.RemoveAt((int)i);
+            }
+
+        }
+    }
+}
